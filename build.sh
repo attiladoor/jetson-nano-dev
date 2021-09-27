91ee6842eee1388276b0f7371d61644b4555d8eb
Attila Door <attila.door@zenseact.com>
2021-09-27 07:42:10 +0200

    Fix cuda installation

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ elif [ $1 == "x86" ]
 then
     BASE="ubuntu:18.04"
     ENABLE_REALSENSE_CUDA="false"
-    OPENCV_CUDA="OFF"
+    ENABLE_OPENCV_CUDA=="OFF"
     ARCH="x86"
 else
     echo "Wrong architecture:" $1
@@ -22,7 +22,7 @@ CONTAINER_NAME="attiladoor/jetson-nano-dev-"
 docker build \
     --build-arg BASE=$BASE \
     --build-arg ENABLE_REALSENSE_CUDA=$REALSENSE_CUDA \
-    --build-arg ENABLE_OPENCV_CUDA=$OPENCV_CUDA \
+    --build-arg ENABLE_OPENCV_CUDA=$ENABLE_OPENCV_CUDA \
     --build-arg ARCH=$ARCH \
     --tag $CONTAINER_NAME$1 \
     --file docker/Dockerfile .
