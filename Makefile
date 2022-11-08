a8d9d97aa70f9d91d51cbf46f905adc16cbb1985
Attila Door <attiladoor@gmail.com>
2022-11-08 21:04:46 +0100

    Add version option and remake Makefile

--- Makefile
+++ Makefile
@@ -1,28 +1,37 @@
 default: all
 
 arm-18-build:
-	bash ./build.sh arm 18
+	bash ./build.sh build arm 18
 arm-20-build:
-	bash ./build.sh arm 20
+	bash ./build.sh build arm 20
 x86-18-build:
-	bash ./build.sh x86 18 build
+	bash ./build.sh build x86 18 build
 x86-20-build:
-	bash ./build.sh x86 20 build
+	bash ./build.sh build x86 20 build
 x86-18-run: x86-18-build
-	bash ./build.sh x86 18 run
+	bash ./build.sh build x86 18 run
 x86-20-run: x86-20-build
-	bash ./build.sh x86 20 run
+	bash ./build.sh build x86 20 run
 
-all: arm-20 x86-20
+build-all-20: arm-20-build x86-20-run
+build-all-18: arm-18-build x86-18-run
+build-all: build-all-20 build-all-18
 
-push-arm-18:
-	docker push attiladoor/jetson-nano-dev-arm-18:latest
-push-arm-20:
-	docker push attiladoor/jetson-nano-dev-arm-20:latest
-push-x86-18:
-	docker push attiladoor/jetson-nano-dev-x86-18:latest
-push-x86-20:
-	docker push attiladoor/jetson-nano-dev-x86-20:latest
-push-all: push-arm-20 push-x86-20
+push-arm-18-build:
+	bash ./build.sh push arm 18 build
+push-arm-20-build:
+	bash ./build.sh push arm 20 build
+push-x86-18-build:
+	bash ./build.sh push x86 18 build
+push-x86-20-build:
+	bash ./build.sh push x86 20 build
+push-x86-18-run:
+	bash ./build.sh push x86 18 run
+push-x86-20-run:
+	bash ./build.sh push x86 20 run
 
+push-all-20: push-arm-20-build push-x86-20-build push-x86-20-run
+push-all-18: push-arm-18-build push-x86-18-build push-x86-18-run
+push-all: push-all-18 push-all-20
 
+all: build-all push-all
--- build.sh
+++ build.sh
@@ -1,55 +1,55 @@
 set -e
 
 CONTAINER_BASE_NAME="attiladoor/jetson-nano-dev-"
+DOCKER_COMMAND=$1
+ARCH=$2
+UBUNTU_VERSION=$3
+VARIANT_OPT=$4
 
-if [ $1 == "arm" ]
+if [ $ARCH == "arm" ]
 then
-    if [ ! -z $3 ]
-    then
-        echo "Invalid positional parameter: ${3}"
-        exit 1
-    fi
+    # if [ ! -z $VARIANT_OPT ]
+    # then
+    #     echo "Invalid positional parameter: ${VARIANT_OPT}"
+    #     exit 1
+    # fi
 
-    if [ $2 == "18" ]
+    if [ $UBUNTU_VERSION == "18" ]
     then
         BASE="nvcr.io/nvidia/l4t-base:r32.6.1"
-    elif [ $2 == "20" ]
+    elif [ $UBUNTU_VERSION == "20" ]
     then
         BASE="arm64v8/ubuntu:20.04"
     else
-        echo "Wrong ubuntu version:" $2
+        echo "Wrong ubuntu version:" $UBUNTU_VERSION
         exit 1
     fi
     ENABLE_CUDA="ON"
-    ARCH="arm"
     DOCKER_FILE="docker-build/Dockerfile"
-elif [ $1 == "x86" ]
+elif [ $ARCH == "x86" ]
 then
-    if [ $2 == "18" ]
+    if [ $UBUNTU_VERSION == "18" ]
     then
         BASE="ubuntu:18.04"
-        VERSION="18"
-    elif [ $2 == "20" ]
+    elif [ $UBUNTU_VERSION == "20" ]
     then
         BASE="ubuntu:20.04"
-        VERSION="20"
     else
-        echo "Wrong ubuntu version:" $2
+        echo "Wrong ubuntu version:" $UBUNTU_VERSION
         exit 1
     fi
 
     ARCH="x86"
 
-
-    if [ -z $3 ]
+    if [ -z $VARIANT_OPT ]
     then
         echo "Container variant should be specified"
         exit 1
-    elif [ $3 == "run" ]
+    elif [ $VARIANT_OPT == "run" ]
     then
         VARIANT_TAG="-run"
-        BASE="${CONTAINER_BASE_NAME}${ARCH}-${VERSION}-build"
-    elif [ $3 == "build" ]
+        BASE="${CONTAINER_BASE_NAME}${ARCH}-${UBUNTU_VERSION}-build:${VERSION}"
+    elif [ $VARIANT_OPT == "build" ]
     then
         VARIANT_TAG="-build"
     fi
@@ -58,13 +58,27 @@ then
     ENABLE_CUDA="OFF"
 
 else
-    echo "Wrong architecture:" $1
+    echo "Wrong architecture:" $ARCH
     exit 1
 fi
 
-docker build \
-    --build-arg BASE=$BASE \
-    --build-arg ENABLE_CUDA=$ENABLE_CUDA \
-    --build-arg ARCH=$ARCH \
-    --tag $CONTAINER_BASE_NAME$ARCH-$VERSION$VARIANT_TAG \
-    --file $DOCKER_FILE .
+if [ -z $VERSION ]
+then
+    VERSION=latest
+fi
+
+if [[ $DOCKER_COMMAND == "build" ]]
+then
+    docker buildx build \
+        --build-arg BASE=$BASE \
+        --build-arg ENABLE_CUDA=$ENABLE_CUDA \
+        --build-arg ARCH=$ARCH \
+        --tag $CONTAINER_BASE_NAME$ARCH-$UBUNTU_VERSION$VARIANT_TAG:$VERSION \
+        --file $DOCKER_FILE .
+elif [[ $DOCKER_COMMAND == "push" ]]
+then
+echo $CONTAINER_BASE_NAME$ARCH-$UBUNTU_VERSION$VARIANT_TAG:$VERSION
+docker push $CONTAINER_BASE_NAME$ARCH-$UBUNTU_VERSION$VARIANT_TAG:$VERSION
+else
+echo "Invalid docker command $DOCKER_COMMAND"
+fi
